36cbe7be7660c534625a9afe56fd70cbc7d8c716
michael_prog <michael_prog@mail.ru>
2019-03-03 15:29:14 +0300

    Добавил условия TeamMembersCount и TeamMemberCountInCustomRegion

---
+++ EntityPlugin/Conditions/TeamMemberCount.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.ComponentModel;
+using Astral.Quester.Classes;
+using MyNW.Classes;
+using MyNW.Internals;
+
+namespace EntityPlugin.Conditions
+{
+    [Serializable]
+    public class TeamMembersCount : Condition
+    {
+        public TeamMembersCount()
+        {
+            MemberCount = 3;
+            Distance = 50;
+            Sign = Relation.Inferior;
+            DistanceSign = Relation.Inferior;
+        }
+
+        public override void Reset()
+        {
+        }
+
+        public float Distance { get; set; }
+
+        [Description("Distance comparison type to the closest Entity")]
+        public Relation DistanceSign { get; set; }
+
+        public int MemberCount { get; set; }
+
+        public Relation Sign { get; set; }
+
+        public override bool IsValid
+        {
+            get
+            {
+                int membersCount = 0;
+                switch (DistanceSign)
+                {
+                    case Relation.Inferior:
+                        {
+                            membersCount = EntityManager.LocalPlayer.PlayerTeam.Team.Members.FindAll((TeamMember member) =>
+                                            (   member.InternalName != EntityManager.LocalPlayer.InternalName && 
+                                                member.Entity.Location.Distance3DFromPlayer < Distance)
+                                            ).Count;                            
+
+                            break;
+                        }
+                    case Relation.Superior:
+                        {
+                            membersCount = EntityManager.LocalPlayer.PlayerTeam.Team.Members.FindAll((TeamMember member) =>
+                                            (   member.InternalName != EntityManager.LocalPlayer.InternalName && 
+                                                member.Entity.Location.Distance3DFromPlayer > Distance)
+                                            ).Count;
+                            break;
+                        }
+                    case Relation.Equal:
+                        {
+                            membersCount = EntityManager.LocalPlayer.PlayerTeam.Team.Members.FindAll((TeamMember member) =>
+                                            (   member.InternalName != EntityManager.LocalPlayer.InternalName && 
+                                                member.Entity.Location.Distance3DFromPlayer == Distance)
+                                            ).Count;
+                            break;
+                        }
+                    case Relation.NotEqual:
+                        {
+                            membersCount = EntityManager.LocalPlayer.PlayerTeam.Team.Members.FindAll((TeamMember member) =>
+                                            (   member.InternalName != EntityManager.LocalPlayer.InternalName && 
+                                                member.Entity.Location.Distance3DFromPlayer != Distance)
+                                            ).Count;
+                            break;
+                        }
+                }
+                bool result;
+                switch (Sign)
+                {
+                    case Condition.Relation.Equal:
+                        result = ((ulong)membersCount == (ulong)((long)MemberCount));
+                        break;
+                    case Condition.Relation.NotEqual:
+                        result = ((ulong)membersCount != (ulong)((long)MemberCount));
+                        break;
+                    case Condition.Relation.Inferior:
+                        result = ((ulong)membersCount < (ulong)((long)MemberCount));
+                        break;
+                    case Condition.Relation.Superior:
+                        result = ((ulong)membersCount > (ulong)((long)MemberCount));
+                        break;
+                    default:
+                        result = false;
+                        break;
+                }
+                return result;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{GetType().Name} {Sign} to {MemberCount} which Distance {DistanceSign} to {Distance}";
+        }
+
+        public override string TestInfos
+        {
+            get
+            {
+                if (EntityManager.LocalPlayer.PlayerTeam.IsInTeam)
+                {
+                    int membersCount = 0;
+
+                    switch (DistanceSign)
+                    {
+                        case Relation.Inferior:
+                            {
+                                membersCount = EntityManager.LocalPlayer.PlayerTeam.Team.Members.FindAll((TeamMember member) => 
+                                        (   member.InternalName != EntityManager.LocalPlayer.InternalName && 
+                                            member.Entity.Location.Distance3DFromPlayer < Distance )    
+                                        ).Count;
+                                break;
+                            }
+                        case Relation.Superior:
+                            {
+                                membersCount = EntityManager.LocalPlayer.PlayerTeam.Team.Members.FindAll((TeamMember member) => 
+                                        (   member.InternalName != EntityManager.LocalPlayer.InternalName && 
+                                            member.Entity.Location.Distance3DFromPlayer > Distance)
+                                        ).Count;
+                                break;
+                            }
+                        case Relation.Equal:
+                            {
+                                membersCount = EntityManager.LocalPlayer.PlayerTeam.Team.Members.FindAll((TeamMember member) =>
+                                        (   member.InternalName != EntityManager.LocalPlayer.InternalName &&
+                                            member.Entity.Location.Distance3DFromPlayer == Distance)
+                                        ).Count;
+                                break;
+                            }
+                        case Relation.NotEqual:
+                            {
+                                membersCount = EntityManager.LocalPlayer.PlayerTeam.Team.Members.FindAll((TeamMember member) =>
+                                        (   member.InternalName != EntityManager.LocalPlayer.InternalName &&
+                                            member.Entity.Location.Distance3DFromPlayer != Distance)
+                                        ).Count;
+                                break;
+                            }
+                    }
+
+                    return $"{membersCount} TeamMember has Distance from Player {DistanceSign} to {Distance}";
+                }
+                else
+                {
+                    return "Player is not in a party";
+                }
+            }
+        }
+    }
+}
---
+++ EntityPlugin/Conditions/TeamMemberCountInCustomRegion.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing.Design;
+using Astral.Quester.Classes;
+using Astral.Quester.UIEditors;
+using MyNW.Classes;
+using MyNW.Internals;
+
+namespace EntityPlugin.Conditions
+{
+    /// <summary>
+    /// Проверка наличия хотя бы одного члена группы (но не игрока)
+    /// в регионе CustomRegion, заданным в CustomRegionName
+    /// </summary>
+
+    [Serializable]
+    public class TeamMemberCountInCustomRegion : Condition
+    {
+        public Presence Tested { get; set; }
+
+        [Editor(typeof(CustomRegionEditor), typeof(UITypeEditor))]
+        public string CustomRegionName { get; set; }
+
+        public int MemberCount { get; set; }
+
+        public Relation Sign { get; set; }
+
+        public TeamMemberCountInCustomRegion()
+        {
+            MemberCount = 3;
+            Sign = Relation.Inferior;
+
+            Tested = Presence.Equal;
+            foreach (CustomRegion customRegion in Astral.Quester.Core.Profile.CustomRegions)
+            {
+                if (customRegion.IsIn)
+                {
+                    CustomRegionName = customRegion.Name;
+                    break;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return "CustomRegion " + Tested + " " + CustomRegionName;
+        }
+
+        public override bool IsValid
+        {
+            get
+            {
+                if (EntityManager.LocalPlayer.PlayerTeam.IsInTeam)
+                {
+                    uint memCount = 0;
+                    CustomRegion customRegion = Astral.Quester.Core.Profile.CustomRegions.Find(CR => CR.Name == CustomRegionName);
+
+                    foreach (TeamMember member in EntityManager.LocalPlayer.PlayerTeam.Team.Members)
+                    {
+                        if (member.InternalName != EntityManager.LocalPlayer.InternalName && Tools.IsInCustomRegion(member.Entity, customRegion))
+                            memCount++;
+                    }
+
+                    if (Tested == Presence.NotEquel)
+                        memCount = EntityManager.LocalPlayer.PlayerTeam.Team.MembersCount - memCount;
+
+                    switch (Sign)
+                    {
+                        case Relation.Equal:
+                            return memCount == MemberCount;
+                        case Relation.NotEqual:
+                            return memCount != MemberCount;
+                        case Relation.Inferior:
+                            return memCount < MemberCount;
+                        case Relation.Superior:
+                            return memCount > MemberCount;
+                    }
+                }
+                return false;
+            }
+        }
+
+        public override string TestInfos
+        {
+            get
+            {
+                if (EntityManager.LocalPlayer.PlayerTeam.IsInTeam)
+                {
+                    uint memCount = 0;
+                    CustomRegion customRegion = Astral.Quester.Core.Profile.CustomRegions.Find(CR => CR.Name == CustomRegionName);
+
+                    foreach (TeamMember member in EntityManager.LocalPlayer.PlayerTeam.Team.Members)
+                    {
+                        if (member.InternalName != EntityManager.LocalPlayer.InternalName && Tools.IsInCustomRegion(member.Entity, customRegion))
+                            memCount++;
+                    }
+
+                    if (Tested == Presence.NotEquel)
+                        memCount = EntityManager.LocalPlayer.PlayerTeam.Team.MembersCount - memCount;
+
+
+                    return $"{memCount} TeamMembers are {Tested} in CustomRegion '{CustomRegionName}'";
+                }
+                return "Player is not in a party";
+            }
+        }
+
+        public override void Reset()
+        {
+        }
+    }
+}
