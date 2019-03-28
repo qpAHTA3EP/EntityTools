d20945a13bbee1b494af919ee334b34fcce29fb1
michael_prog <michael_prog@mail.ru>
2019-03-28 12:20:56 +0300

    Исправил MultiCustomRegionSelectEditor, для отображения списка ранее выбранных CustomRegion в окне выбора (при редактировании свойства CustomRegions условий EntityCountInCustomRegions и TeamMembersCountInCustomRegions)

--- EntityPlugin/Editors/MultiCustomRegionSelectEditor.cs
+++ EntityPlugin/Editors/MultiCustomRegionSelectEditor.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Drawing.Design;
 using System.Windows.Forms;
 using Astral.Quester.Classes;
+using DevExpress.XtraEditors.Controls;
 using EntityPlugin.Forms;
 using MyNW.Classes;
 
@@ -19,21 +20,43 @@ namespace EntityPlugin.Editors
             if (listEditor == null)
                 listEditor = new Astral.Quester.UIEditors.Forms.SelectList();
 
+            // Настройка окна для отображения списка CustomRegion
             listEditor.Text = "CustomRegionSelect";
-            listEditor.listitems.DataSource = Astral.Quester.API.CurrentProfile.CustomRegions;
-            listEditor.listitems.DisplayMember = "Name";
-            listEditor.listitems.ToolTip = "Hold 'Ctrl' or 'Shift' key to select several CustomRegions";            
+            listEditor.listitems.DataSource = null;//= Astral.Quester.API.CurrentProfile.CustomRegions;
+            listEditor.listitems.DisplayMember = string.Empty;//"Name";
+            listEditor.listitems.ToolTip = "Hold 'Ctrl' or 'Shift' key to select several CustomRegions";
 
+            List<string> regions = value as List<string>;
+
+            // Заполнение списка регионов
+            listEditor.listitems.Items.Clear();
+            foreach(CustomRegion cr in Astral.Quester.API.CurrentProfile.CustomRegions)
+            {
+                int ind = listEditor.listitems.Items.Add(cr.Name);
+                if(regions!= null && regions.Contains(cr.Name))
+                    listEditor.listitems.SetSelected(ind, true);
+                else listEditor.listitems.SetSelected(ind, false);
+            }
+
+            // Отображение списка CustomRegion пользователю
             DialogResult dialogResult = listEditor.ShowDialog();
-            List<string> regions = new List<string>();
 
             if (dialogResult == DialogResult.OK && listEditor.listitems.SelectedItems.Count > 0)
             {
-                foreach(object item in listEditor.listitems.SelectedItems)
+                // Формирование списка выбранных CustomRegion
+                regions.Clear();
+                foreach (object item in listEditor.listitems.SelectedItems)
                 {
-                    CustomRegion cr = item as CustomRegion;
-                    if (cr != null)
-                        regions.Add(cr.Name);
+                    // Формирование списка с привязкой данных 
+                    //CustomRegion cr = item as CustomRegion;
+                    //if (cr != null)
+                    //    regions.Add(cr.Name);
+
+                    // Формирование списка без привязкой данных 
+                    if (item is string crName)
+                    {
+                        regions.Add(crName);
+                    }
                 }
                 return regions;
             }
