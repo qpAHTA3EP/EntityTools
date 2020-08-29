4bb4ee8da37d79429a4653177c48fd2fc4e26dec
michael_prog <michael_prog@mail.ru>
2020-08-29 23:40:38 +0300

    Добавил функтор доступа к Astral.Quester.FSM.States.Combat.ignoreCombatMinHP

--- EntityTools/Tools/Reflection/AstralAccessors.cs
+++ EntityTools/Tools/Reflection/AstralAccessors.cs
@@ -23,9 +23,10 @@ namespace EntityTools.Tools.Reflection
             internal static readonly Func<string, Entity> GetNearestEntityByCostume = typeof(Astral.Logic.NW.VIP).GetStaticFunction<string, Entity>("GetNearestEntityByCostume");
         }
 
-#if false
+
         internal static class Quester
         {
+#if false
             internal static class Action
             {
                 internal static readonly InstancePropertyAccessor<Astral.Quester.Classes.Action, Astral.Quester.Classes.ActionDebug> ActionDebug = null;
@@ -34,9 +35,23 @@ namespace EntityTools.Tools.Reflection
                 {
                     ActionDebug = typeof(Astral.Quester.Classes.Action).GetInstanceProperty<Astral.Quester.Classes.ActionDebug>("Debug");
                 }
+            } 
+#endif
+            //Astral.Quester.FSM.States.Combat.ignoreCombatMinHP
+
+            internal static class FSM
+            {
+                internal static class States
+                {
+                    internal static class Combat
+                    {
+                        internal static readonly StaticFieldAccessor<int> IgnoreCombatMinHP =
+                            typeof(Astral.Quester.FSM.States.Combat).GetStaticField<int>("ignoreCombatMinHP");
+                    }
+                }
             }
         } 
-#endif
+
         /// <summary>
         /// Доступ к членам и методам класса
         /// Astral.Classes.ItemFilter
