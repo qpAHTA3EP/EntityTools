1514b6ea02e8ca8253f8c2f84ba671277c2a39bd
michael_prog <michael_prog@mail.ru>
2021-02-17 08:53:17 +0300

    Убрал ограничение буфера ETLogger'a

--- EntityTools/Logger/EntityToolLogger.cs
+++ EntityTools/Logger/EntityToolLogger.cs
@@ -9,29 +9,27 @@ namespace EntityTools
 {
     internal class ETLogger
     {
-        private static StringBuilder LogCache = new StringBuilder(1024);
-        internal static string LogFilePath = string.Empty;
+        private static readonly object Locker = new object();
+        private static readonly StringBuilder LogCache = new StringBuilder();
+        internal static string LogFilePath => logFilePath;
+        private static string logFilePath = string.Empty;
 
         private static CancellationTokenSource cancellationTokenSource;
-        private static CancellationToken cancellationToken;
-        private static Task backgroundLoggerTask;
+        private static Task loggerTask;
 
         internal static void Start()
         {
-            if (backgroundLoggerTask?.Status != TaskStatus.Running
-                && EntityTools.Config.Logger.Active)
-            {
-                LogFilePath = string.Concat(EntityTools.Config.Logger.LogsPath, DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"),
-                                            //DateTime.Now.Year.ToString("00"), "-", DateTime.Now.Month.ToString("00"), "-", DateTime.Now.Day.ToString("00"), "_", 
-                                            //DateTime.Now.Hour.ToString("00"), "-", DateTime.Now.Minute.ToString("00"), "-", DateTime.Now.Second.ToString("00"), 
-                                            ".log");
-                if (!Directory.Exists(EntityTools.Config.Logger.LogsPath))
-                    Directory.CreateDirectory(EntityTools.Config.Logger.LogsPath);
+            if (loggerTask?.Status == TaskStatus.Running || !EntityTools.Config.Logger.Active) return;
 
-                cancellationTokenSource = new CancellationTokenSource();
-                cancellationToken = cancellationTokenSource.Token;
-                backgroundLoggerTask = Task.Factory.StartNew(Run, cancellationToken);
-            }
+            logFilePath = string.Concat(EntityTools.Config.Logger.LogsPath, DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"),
+                //DateTime.Now.Year.ToString("00"), "-", DateTime.Now.Month.ToString("00"), "-", DateTime.Now.Day.ToString("00"), "_", 
+                //DateTime.Now.Hour.ToString("00"), "-", DateTime.Now.Minute.ToString("00"), "-", DateTime.Now.Second.ToString("00"), 
+                ".log");
+            if (!Directory.Exists(EntityTools.Config.Logger.LogsPath))
+                Directory.CreateDirectory(EntityTools.Config.Logger.LogsPath);
+
+            cancellationTokenSource = new CancellationTokenSource();
+            loggerTask = Task.Factory.StartNew(() => Run(cancellationTokenSource.Token), cancellationTokenSource.Token);
         }
 
         public static void Stop()
@@ -39,58 +37,66 @@ namespace EntityTools
             cancellationTokenSource.Cancel();
         }
 
-        public static void WriteLine(string Text, bool toAstral = false)
+        public static void WriteLine(string text, bool toAstral = false)
         {
             if(EntityTools.Config.Logger.Active)
-                WriteLine(LogType.Log, Text, toAstral);
+                WriteLine(LogType.Log, text, toAstral);
         }
 
-        public static void WriteLine(LogType logType, string Text, bool toAstral = false)
+        public static void WriteLine(LogType logType, string text, bool toAstral = false)
         {
             if (EntityTools.Config.Logger.Active)
             {
                 try
                 {
-
-                    //LogCache.Append('[').Append(DateTime.Now.Hour.ToString("00")).Append('-').Append(DateTime.Now.Minute.ToString("00")).Append('-').Append(DateTime.Now.Second.ToString("00"));
-                    LogCache.Append(DateTime.Now.ToString("[HH:mm:ss]"));
-                    switch(logType)
+                    lock (Locker)
                     {
-                        case LogType.Log:
-                            LogCache.Append(" [LOG] ");
-                            if (toAstral)
-                                Logger.WriteLine(Logger.LogType.Log, Text);
-                            break;
-                        case LogType.Debug:
-                            LogCache.Append(" [DBG] ");
-                            if (toAstral)
-                                Logger.WriteLine(Logger.LogType.Debug, Text);
-                            break;
-                        case LogType.Error:
-                            LogCache.Append(" [ERR] ");
-                            if (toAstral)
-                                Logger.WriteLine(Logger.LogType.Debug, Text);
-                            break;
+                        LogCache.Append(DateTime.Now.ToString("[HH:mm:ss]"));
+                        switch (logType)
+                        {
+                            case LogType.Log:
+                                LogCache.Append(" [LOG] ");
+                                if (toAstral)
+                                    Logger.WriteLine(Logger.LogType.Log, text);
+                                break;
+                            case LogType.Debug:
+                                LogCache.Append(" [DBG] ");
+                                if (toAstral)
+                                    Logger.WriteLine(Logger.LogType.Debug, text);
+                                break;
+                            case LogType.Error:
+                                LogCache.Append(" [ERR] ");
+                                if (toAstral)
+                                    Logger.WriteLine(Logger.LogType.Debug, text);
+                                break;
+                        }
+
+                        LogCache.AppendLine(text);
                     }
-                    LogCache.AppendLine(Text);
                 }
-                catch { }
+                catch(Exception e)
+                {
+                    File.AppendAllText(logFilePath, DateTime.Now.ToString("[HH:mm:ss] [ERR]") + e);
+                }
             }
         }
 
-        private static void Run()
+        private static void Run(CancellationToken token)
         {
             if (!string.IsNullOrEmpty(LogFilePath))
             {
                 while (EntityTools.Config.Logger.Active
-                       && !cancellationToken.IsCancellationRequested)
+                       && !token.IsCancellationRequested)
                 {
                     try
                     {
-                        if (LogCache.Length > 0)
+                        lock (Locker)
                         {
-                            File.AppendAllText(LogFilePath, LogCache.ToString());
-                            LogCache.Clear();
+                            if (LogCache.Length > 0)
+                            {
+                                File.AppendAllText(logFilePath, LogCache.ToString());
+                                LogCache.Clear();
+                            } 
                         }
                     }
                     catch (Exception ex)
@@ -114,7 +120,7 @@ namespace EntityTools
         /// </summary>
         Debug,
         /// <summary>
-        /// Сообщени об ошибке
+        /// Сообщение об ошибке
         /// </summary>
         Error
     }
