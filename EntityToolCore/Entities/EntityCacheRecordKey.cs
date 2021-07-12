daa2b25c04fdbd3d046accec90e2d61462abaeb7
michael_prog <michael_prog@mail.ru>
2021-07-12 23:04:30 +0300

    Добавил ленивую инициализацию компаратора со ссылкой на конструктор предиката EntityComparer.Get

--- EntityToolCore/Entities/EntityCacheRecordKey.cs
+++ EntityToolCore/Entities/EntityCacheRecordKey.cs
@@ -1,31 +1,21 @@
 ﻿using Astral.Classes.ItemFilter;
-using EntityTools;
 using EntityTools.Enums;
 using EntityTools.Extensions;
 using MyNW.Classes;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace EntityCore.Entities
 {
     public class EntityCacheRecordKey
     {
-        public EntityCacheRecordKey()
-        {
-            comparer = CompareFalse;
-        }
         public EntityCacheRecordKey(string p, ItemFilterStringType mp = ItemFilterStringType.Simple, EntityNameType nt = EntityNameType.InternalName, EntitySetType est = EntitySetType.Complete)
         {
             Pattern = p;
             MatchType = mp;
             NameType = nt;
             EntitySetType = est;
-            comparer = initialize_Comparer(Pattern, MatchType, NameType);
+            comparer = Comparer_Initializer;
         }
 
 
@@ -40,17 +30,17 @@ namespace EntityCore.Entities
         {
             if (ReferenceEquals(this, otherObj))
                 return true;
-            else if (otherObj is EntityCacheRecordKey other)
+            if (otherObj is EntityCacheRecordKey other)
                 return Equals(other);
             return false;
         }
 
         public bool Equals(EntityCacheRecordKey other)
         {
-            return Pattern == other.Pattern
-                    && MatchType == other.MatchType
-                    && NameType == other.NameType
-                    && EntitySetType == other.EntitySetType;
+            return MatchType == other.MatchType
+                   && NameType == other.NameType
+                   && EntitySetType == other.EntitySetType
+                   && Pattern == other.Pattern;
         }
 
         public override int GetHashCode()
@@ -62,114 +52,32 @@ namespace EntityCore.Entities
         /// <summary>
         /// Проверка 
         /// </summary>
-        public bool IsMatch(Entity e)
+        public bool IsMatch(Entity entity)
         {
-            if (e is null) return false;
-            return comparer(e);
+            if (entity is null) return false;
+            return comparer(entity);
         }
 
-        internal bool Validate(Entity e)
+        internal bool Validate(Entity entity)
         {
-            return e != null && e.IsValid
-                    && (e.Character.IsValid || e.Critter.IsValid || e.Player.IsValid)
-                    && comparer(e);
+            return entity != null && entity.IsValid
+                    && (entity.Character.IsValid || entity.Critter.IsValid || entity.Player.IsValid)
+                    && comparer(entity);
         }
 
-        private readonly Predicate<Entity> comparer = null;
-        string treatedPattern;
+        private Predicate<Entity> comparer;
+
         /// <summary>
-        /// Метод, инициализирующий функтор <see cref="comparer"/>,
-        /// использующийся для проверки сущности <seealso cref="Entity"/> на соответствия сочетанию признаков <param name="entityId"/>, <param name="matchType"/>, <param name="entityNameType"/>
+        /// Метод ленивой инициализации компаратора <see cref="comparer"/>, который выполняет сопоставление <param name="entity"/> с заданным набором атрибутов <see cref="Pattern"/>, <see cref="MatchType"/> и <see cref="NameType"/>
         /// </summary>
-        private Predicate<Entity> initialize_Comparer(string entityId, ItemFilterStringType matchType = ItemFilterStringType.Simple, EntityNameType entityNameType = EntityNameType.NameUntranslated)
+        private bool Comparer_Initializer(Entity entity)
         {
-            //bool extendedDebugInfo = EntityTools.EntityTools.Config.Logger.DebugEntityTools;
-            //string currentMethodName = extendedDebugInfo ? string.Concat(GetType().Name, '.', MethodBase.GetCurrentMethod().Name) : string.Empty;
+            var predicate = EntityComparer.Get(Pattern, MatchType, NameType);
+            
+            comparer = predicate ?? (ett => false);
 
-            Predicate<Entity> predicate;
-            treatedPattern = string.Empty;
-
-            if (matchType == ItemFilterStringType.Simple)
-            {
-                if (entityId == "*" || entityId == "**")
-                    predicate = CompareSimpleAny;
-                else
-                {
-                    SimplePatternPos pos = entityId.GetSimplePatternPosition(out treatedPattern);
-
-                    if (entityNameType == EntityNameType.InternalName)
-                        switch (pos)
-                        {
-                            case SimplePatternPos.Full:
-                                predicate = CompareInternal2SimpleFull;
-                                break;
-                            case SimplePatternPos.Start:
-                                predicate = CompareInternal2SimpleStart;
-                                break;
-                            case SimplePatternPos.Middle:
-                                predicate = CompareInternal2SimpleMiddle;
-                                break;
-                            case SimplePatternPos.End:
-                                predicate = CompareInternal2SimpleEnd;
-                                break;
-                            default:
-                                predicate = CompareFalse;
-                                break;
-                        }
-                    else if (entityNameType == EntityNameType.NameUntranslated)
-                        switch (pos)
-                        {
-                            case SimplePatternPos.Full:
-                                predicate = CompareUntranslated2SimpleFull;
-                                break;
-                            case SimplePatternPos.Start:
-                                predicate = CompareUntranslated2SimpleStart;
-                                break;
-                            case SimplePatternPos.Middle:
-                                predicate = CompareUntranslated2SimpleMiddle;
-                                break;
-                            case SimplePatternPos.End:
-                                predicate = CompareUntranslated2SimpleEnd;
-                                break;
-                            default:
-                                predicate = CompareFalse;
-                                break;
-                        }
-                    else predicate = CompareEmpty;
-                }
-            }
-            else
-            {
-                if (entityNameType == EntityNameType.InternalName)
-                    predicate = CompareInternal2Regex;
-                else if (entityNameType == EntityNameType.NameUntranslated)
-                    predicate = CompareUntranslated2Regex;
-                else predicate = CompareEmpty;
-            }
-
-            return predicate;
+            return comparer(entity);
         }
-
-        private bool CompareInternal2SimpleFull(Entity e) => e.InternalName.Equals(treatedPattern);
-        private bool CompareUntranslated2SimpleFull(Entity e) => e.NameUntranslated.Equals(treatedPattern);
-
-        private bool CompareInternal2SimpleStart(Entity e) => e.InternalName.StartsWith(treatedPattern);
-        private bool CompareUntranslated2SimpleStart(Entity e) => e.NameUntranslated.StartsWith(treatedPattern);
-
-        private bool CompareInternal2SimpleMiddle(Entity e) => e.InternalName.Contains(treatedPattern);
-        private bool CompareUntranslated2SimpleMiddle(Entity e) => e.NameUntranslated.Contains(treatedPattern);
-
-        private bool CompareInternal2SimpleEnd(Entity e) => e.InternalName.EndsWith(treatedPattern);
-        private bool CompareUntranslated2SimpleEnd(Entity e) => e.NameUntranslated.EndsWith(treatedPattern);
-
-        private bool CompareSimpleAny(Entity e) => true;
-
-        private bool CompareInternal2Regex(Entity e) => Regex.IsMatch(e.InternalName, Pattern);
-        private bool CompareUntranslated2Regex(Entity e) => Regex.IsMatch(e.NameUntranslated, Pattern);
-
-        private bool CompareEmpty(Entity e) => string.IsNullOrEmpty(e.InternalName) && string.IsNullOrEmpty(e.NameUntranslated);
-
-        private bool CompareFalse(Entity e) => false;
         #endregion
     }
 }
