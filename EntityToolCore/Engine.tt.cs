c59620fbcbaea8198a24c86c825ccb3bd4bc5853
DESKTOP-9QDF3HG\STALKER <bakalo.ma@medotrade.ru>
2022-04-17 23:21:15 +0300

    Добавил директиву пересборки T4 шаблонов для корректной валидации Engine

--- EntityToolCore/Engine.tt.cs
+++ EntityToolCore/Engine.tt.cs
@@ -5,8 +5,10 @@ namespace EntityCore
     {
         /// <summary>
         /// Правая часть неравенства представляет собой дату, через 87 дней после даты компиляции
-        /// 87 дней фактический лимит действия сборки
+        /// 87 дней фактический лимит действия сборки.
+        /// Чтобы шаблон обрабатывался при каждом билде в Prebuild Event нужно добавить директиву:
+        /// <code>"$(DevEnvDir)TextTransform.exe" -out "$(ProjectDir)Engine.tt.cs" "$(ProjectDir)Engine.tt"</code>
         /// </summary>
-        private static readonly bool isValid = DateTime.Now.Ticks <= 637858263010010697L;
+        private static readonly bool isValid = DateTime.Now.Ticks <= 637933501340514839L;
     }
 }
