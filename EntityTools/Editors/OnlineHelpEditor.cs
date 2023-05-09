feddde5c5ce7222eee1db6e933ad5b8d8569215d
qpAHTA3EP <michael_prog@mail.ru>
2023-05-09 23:09:17 +0300

    Добавил ucc-команду ExecuteConsoleCommand

---
+++ EntityTools/Editors/OnlineHelpEditor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing.Design;
+
+namespace EntityTools.Editors
+{
+    public class OnlineHelpEditor : UITypeEditor
+    {
+        public override object EditValue(ITypeDescriptorContext context, IServiceProvider provider, object value)
+        {
+            if (context?.PropertyDescriptor?.Attributes[typeof(OnlineHelpAttribute)] is OnlineHelpAttribute attribute
+                && !string.IsNullOrWhiteSpace(attribute.Url))
+            {
+                Process.Start(attribute.Url); ;
+            }
+            return value;
+        }
+
+        public override UITypeEditorEditStyle GetEditStyle(ITypeDescriptorContext context)
+        {
+            return UITypeEditorEditStyle.Modal;
+        }
+    }
+
+    public class OnlineHelpAttribute : Attribute
+    {
+        public OnlineHelpAttribute(string url)
+        {
+            Url = url;
+        }
+
+        public string Url { get; set; }
+    }
+}
---
+++ EntityTools/UCC/Actions/ExecuteConsoleCommand.cs
@@ -0,0 +1,82 @@
+﻿using Astral.Logic.UCC.Classes;
+using EntityTools.Editors.TestEditors;
+using Infrastructure.Annotations;
+using System;
+using System.ComponentModel;
+using System.Drawing.Design;
+using System.Runtime.CompilerServices;
+using System.Xml.Serialization;
+using EntityTools.Editors;
+
+namespace EntityTools.UCC.Actions
+{
+    [Serializable]
+    public class ExecuteConsoleCommand : UCCAction, INotifyPropertyChanged
+    {
+        public ExecuteConsoleCommand()
+        {
+            Timer = 500;
+            CoolDown = 2000;
+        }
+
+        [Category("Required")]
+        [Description("The text of the command to be executed within the game console.")]
+        public string Command
+        {
+            get => _command;
+            set
+            {
+                OnPropertyChanged();
+                _command = value;
+            }
+        }
+        private string _command;
+
+        [XmlIgnore]
+        [Category("Required")]
+        [OnlineHelp("https://neverwinter.fandom.com/wiki/Console_command")]
+        [Description("Open online documentation about NeverWinter ConsoleCommand.")]
+        [Editor(typeof(OnlineHelpEditor), typeof(UITypeEditor))]
+        public string OnlineHelp => "https://neverwinter.fandom.com/wiki/Console_command";
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = default)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+
+        public override UCCAction Clone()
+        {
+            return BaseClone(new ExecuteConsoleCommand { _command = _command });
+        }
+
+        public override bool Run()
+        {
+            MyNW.Internals.GameCommands.Execute(_command);
+            return true;
+        }
+
+        public override bool NeedToRun => !string.IsNullOrWhiteSpace(_command);
+
+        public override string ToString()
+        {
+            return nameof (ExecuteConsoleCommand) + ": " + _command;
+        }
+
+        #region Hide Inherited Properties
+        [XmlIgnore]
+        [Browsable(false)]
+        public new string ActionName { get; set; }
+        [XmlIgnore]
+        [Browsable(false)]
+        public new Astral.Logic.UCC.Ressources.Enums.Unit Target  => Astral.Logic.UCC.Ressources.Enums.Unit.Player;
+        [XmlIgnore]
+        [Browsable(false)]
+        public new uint Range { get; set; }
+        #endregion
+    }
+}
